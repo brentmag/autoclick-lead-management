9a5d07cbecac66aa134a0a7dc816b05393a82e84
brentmag <brentmag@gmail.com>
2025-07-13 21:44:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const authenticateToken = (req, res, next) => {
 };
 
 // Email configuration
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
